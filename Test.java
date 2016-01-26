59d25aaed4c40111c585371eb4aee228d97690aa
AilingHou <2228352327@qq.com>
2016-01-26 17:03:26 +0800

    cetshi

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/1/14.
+ */
+public class Test {
+    public static void simpleTest(int a){
+        System.out.println("The integer you input is "+a);
+    }
+    public static void main(String[] args){
+        simpleTest(3);
+    }
+}
